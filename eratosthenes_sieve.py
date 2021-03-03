1eee14b790d98d04ad2910c37818a8b79495b3bc
Imran Kocabiyik <imrankocabiyik@gmail.com>
2021-03-03 16:36:53 +0100

    Adding initial files

---
+++ eratosthenes_sieve.py
@@ -0,0 +1,29 @@
+import argparse
+
+parser = argparse.ArgumentParser(description="Prints prime numbers up to a given limit")
+parser.add_argument("--up_to", type=int, default=100, help="The upper limit for the prime number.")
+args = parser.parse_args()
+
+
+def eratosthenes_sieve(upper_limit):
+    """
+    Returns prime numbers up to a given limit.
+
+    Args:
+        upper_limit (int): The upper limit
+
+    Returns:
+        A list of prime numbers
+    """
+
+    sieve = [n for n in range(2, upper_limit)]
+    primes = []
+
+    while len(sieve)!=0:
+        prime_selected = sieve[0]
+        sieve = [n for n in sieve if n%prime_selected != 0]
+        primes.append(prime_selected)
+
+    print(primes)
+
+eratosthenes_sieve(args.up_to)
---
+++ fibonacci_sequence.py
@@ -0,0 +1,29 @@
+import argparse
+
+parser = argparse.ArgumentParser(description="Prints Fibonacci numbers up to a limit")
+parser.add_argument("--up_to", type=int, default=100, help="The upper limit for the Fibonacci sequence")
+args = parser.parse_args()
+
+def fibonacci_sequence(up_to=100):
+    """
+    Returns fibonacci numbers up to the given limit.
+
+    Args:
+        up_to (int): the upper limit of the fibonacci series
+
+    Returns:
+       A list of fibonacci numbers
+    """
+
+
+    seq = [0,1]
+    while seq[-1] < up_to:
+        fib_next = seq[-1] + seq[-2]
+        seq.append(fib_next)
+
+    if seq[-1] > up_to:
+       seq.remove(fib_next)
+
+    print(seq)
+
+fibonacci_sequence(args.up_to)
