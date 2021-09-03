1d29a72b149e572f097ad8367e4e2f9b272e06a8
Elif <eliffgoktas@gmail.com>
2021-09-03 22:55:27 +0200

    Count using collections.Counter

--- strings.py
+++ strings.py
@@ -5,10 +5,14 @@ class String:
     def reverse(self):
         return self.string[::-1]
 
+    def reverse2(self):
+        return "".join(reversed(self.string))
+
     def string_list_reversed(self):
         return list(reversed(self.string))
 
 s = String("quick fox jumps over the lazy dog")
 
 print('Reversed:', s.reverse())
+print('Reversed, alternative solution:', s.reverse2())
 print('Reversed in list:', s.string_list_reversed())
